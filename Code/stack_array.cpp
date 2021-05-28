8cacde04a72b50b7e12332744c6e114c472704e2
Abhishek Bhatt <47862474+bhattabhi013@users.noreply.github.com>
2021-05-28 11:26:31 +0530

    Add files via upload

---
+++ Code/stack_array.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+  int maxSize = 5;
+  int arr[5];
+  int top = -1;
+
+  bool isEmpty()
+  {
+    if(top == -1){
+      return true;
+    }
+    return false;
+  }
+  void push(int element)
+  {
+    if(top < maxSize-1){
+      ++top;
+      arr[top] = element;
+      return;
+    } 
+    cout << "Overflow \n";
+  }
+  int pop()
+  {
+    if(isEmpty()){
+      cout << "Empty Stack \n";
+      return 0;
+    }
+    int popElement = arr[top];
+    arr[top] = 0;
+    --top;
+    return popElement;
+  }
+  int peek(int index)
+  {
+    // if(index < maxSize){
+    // return arr[index];
+    // }
+
+    if (index <= maxSize)
+    {
+      int newIndex = maxSize - index;
+      return arr[newIndex];
+    }
+    
+    cout << "Index not available \n";
+  }
+  void stackTraversal()
+  {
+    int i =0;
+    cout << "Traversal started \n";
+    while(i < maxSize)
+    {
+      cout << i << "  " << arr[i] << "\n";
+      i++;
+    }
+  }
+
+
+
+
+int main(){
+
+  int operation;
+  int element ;
+  // s.push(10);
+  // s.push(11);
+  // s.push(12);
+  // cout << "popped" << s.pop();
+  // cout << "element at " << s.peek(2) << "\n";
+  // s.stackTraversal();
+  // cout << s.isEmpty();
+
+  cout << "Chooose operation \n 1. push \n 2. pop \n 3. peek \n 4. stack traversal \n 5. end operation \n ";
+  cin >> operation;
+  switch (operation)
+  {
+  case 1:
+  cout << "Element to push \n";
+  cin >> element;
+  push(element);
+    break;
+  case 2:
+  int popedVal;
+  popedVal = pop();
+  cout << "Element popped out  " << popedVal << '\n';
+  break;
+  case 3:
+  int index;
+  cout << "Select index to peek , Please type a number between 1 to 5\n";
+  cin >> index;
+  element = peek(index);
+  cout << "Element at index " << index << "\n" << element << "\n";
+  break;
+  case 4:
+  stackTraversal();
+  case 5:
+  return 0;
+  default:
+    break;
+  }
+  main();
+  return 0;
+}
